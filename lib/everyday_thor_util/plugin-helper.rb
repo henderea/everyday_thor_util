2231b29c0be4b266d18c34dba779997019984c16
Eric Henderson <henderea@gmail.com>
2015-02-16 18:20:04 -0500

    more refactoring

--- lib/everyday_thor_util/plugin-helper.rb
+++ lib/everyday_thor_util/plugin-helper.rb
@@ -58,90 +58,97 @@ module EverydayThorUtil
   end
 
   class SubCommandHelpers
-    class << self
-      def map_flag_opts(v)
-        opts = {}
-        name = v[:options][:name].to_sym
-        copy_opts(opts, v, :desc, :banner, :default, :type, :aliases)
-        copy_opt(opts, v, :required, !v[:options][:default])
-        return name, opts
-      end
+    extend EverydayThorUtil::SubCommandCommonHelpers
+    extend EverydayThorUtil::SubCommandFlagHelpers
+    extend EverydayThorUtil::SubCommandCommandHelpers
+    extend EverydayThorUtil::SubCommandHelperHelpers
+  end
+  module SubCommandCommonHelpers
+    def filter_list(list, parent, &extra)
+      list.select { |v| v[:options][:parent] == parent || (extra && extra.call(v)) }
+    end
+  end
+  module SubCommandFlagHelpers
+    def map_flag_opts(v)
+      opts = {}
+      name = v[:options][:name].to_sym
+      copy_opts(opts, v, :desc, :banner, :default, :type, :aliases)
+      copy_opt(opts, v, :required, !v[:options][:default])
+      return name, opts
+    end
 
-      def copy_opts(opts, v, *opt_name)
-        opt_name.each { |n| copy_opt(opts, v, n) }
-      end
+    def copy_opts(opts, v, *opt_name)
+      opt_name.each { |n| copy_opt(opts, v, n) }
+    end
 
-      def copy_opt(opts, v, opt_name, extra_condition = true)
-        opts[opt_name] = v[:options][opt_name] if v[:options][opt_name] && extra_condition
+    def copy_opt(opts, v, opt_name, extra_condition = true)
+      opts[opt_name] = v[:options][opt_name] if v[:options][opt_name] && extra_condition
+    end
+  end
+  module SubCommandCommandHelpers
+    def create_cmd_class_and_aliases(aliases, command_array_symbol, command_symbol, desc, flag_symbol, helper_symbol, id, long_desc, name, parent_class, short_desc)
+      command_ids = Plugins.get_var command_array_symbol
+      unless command_ids.include?(id)
+        command_ids << id
+        Plugins.set_var command_array_symbol, command_ids
+        create_command_class(command_symbol, desc, flag_symbol, helper_symbol, id, long_desc, name, parent_class, short_desc)
+        aliases.each { |a| create_command_class(command_symbol, desc, flag_symbol, helper_symbol, id, long_desc, a, parent_class, short_desc.gsub(/^\S+(?=\s|$)/, a.gsub(/_/, '-'))) } if aliases
       end
+      true
+    end
 
-      def create_cmd_class_and_aliases(aliases, command_array_symbol, command_symbol, desc, flag_symbol, helper_symbol, id, long_desc, name, parent_class, short_desc)
-        command_ids = Plugins.get_var command_array_symbol
-        unless command_ids.include?(id)
-          command_ids << id
-          Plugins.set_var command_array_symbol, command_ids
-          create_command_class(command_symbol, desc, flag_symbol, helper_symbol, id, long_desc, name, parent_class, short_desc)
-          aliases.each { |a| create_command_class(command_symbol, desc, flag_symbol, helper_symbol, id, long_desc, a, parent_class, short_desc.gsub(/^\S+(?=\s|$)/, a.gsub(/_/, '-'))) } if aliases
-        end
-        true
-      end
-
-      def create_cmd_method_and_aliases(aliases, desc, flag_symbol, id, long_desc, name, parent_class, short_desc, v)
+    def create_cmd_method_and_aliases(aliases, desc, flag_symbol, id, long_desc, name, parent_class, short_desc, v)
+      setup_command(desc, flag_symbol, id, long_desc, parent_class, short_desc)
+      parent_class.create_method(name.to_sym, &v[:block])
+      aliases.each { |a|
         setup_command(desc, flag_symbol, id, long_desc, parent_class, short_desc)
-        parent_class.create_method(name.to_sym, &v[:block])
-        aliases.each { |a|
-          setup_command(desc, flag_symbol, id, long_desc, parent_class, short_desc)
-          parent_class.dup_method a.to_sym, name.to_sym
-        } if aliases
-        true
-      end
+        parent_class.dup_method a.to_sym, name.to_sym
+      } if aliases
+      true
+    end
 
-      def extract_command_info(v)
-        return v[:options][:aliases], v[:options][:desc], v[:options][:id], v[:options][:long_desc], v[:options][:name], v[:options][:short_desc]
-      end
+    def extract_command_info(v)
+      return v[:options][:aliases], v[:options][:desc], v[:options][:id], v[:options][:long_desc], v[:options][:name], v[:options][:short_desc]
+    end
 
-      def filter_list(list, parent, &extra)
-        list.select { |v| v[:options][:parent] == parent || (extra && extra.call(v)) }
-      end
+    def setup_root(flag_symbol, helper_symbol, parent, parent_class)
+      Plugins.get helper_symbol, parent_class, nil unless parent || helper_symbol.nil?
+      Plugins.get flag_symbol, parent_class, nil, true unless parent
+    end
 
-      def setup_root(flag_symbol, helper_symbol, parent, parent_class)
-        Plugins.get helper_symbol, parent_class, nil unless parent || helper_symbol.nil?
-        Plugins.get flag_symbol, parent_class, nil, true unless parent
-      end
+    def create_command_class(command_symbol, desc, flag_symbol, helper_symbol, id, long_desc, name, parent_class, short_desc)
+      command_class = Class.new(Thor)
+      command_class.namespace name
+      Plugins.get helper_symbol, command_class, id unless helper_symbol.nil?
+      Plugins.get flag_symbol, command_class, id, true
+      Plugins.get command_symbol, command_class, id
+      setup_command(desc, flag_symbol, id, long_desc, parent_class, short_desc)
+      parent_class.subcommand name, command_class
+    end
 
-      def create_command_class(command_symbol, desc, flag_symbol, helper_symbol, id, long_desc, name, parent_class, short_desc)
-        command_class = Class.new(Thor)
-        command_class.namespace name
-        Plugins.get helper_symbol, command_class, id unless helper_symbol.nil?
-        Plugins.get flag_symbol, command_class, id, true
-        Plugins.get command_symbol, command_class, id
-        setup_command(desc, flag_symbol, id, long_desc, parent_class, short_desc)
-        parent_class.subcommand name, command_class
-      end
+    def setup_command(desc, flag_symbol, id, long_desc, parent_class, short_desc)
+      Plugins.get flag_symbol, parent_class, id, false
+      parent_class.desc short_desc, desc if short_desc && desc
+      parent_class.long_desc long_desc if long_desc
+    end
+  end
+  module SubCommandHelperHelpers
+    def add_helper(parent_class, v)
+      parent_class.no_commands { parent_class.create_method v[:options][:name].to_sym, &v[:block] } if v[:block]
+    end
 
-      def setup_command(desc, flag_symbol, id, long_desc, parent_class, short_desc)
-        Plugins.get flag_symbol, parent_class, id, false
-        parent_class.desc short_desc, desc if short_desc && desc
-        parent_class.long_desc long_desc if long_desc
-      end
+    def register_print_info_helper(global, helper_symbol, method_name, parent)
+      register(helper_symbol, name: (method_name || 'print_info'), global: global, parent: parent) { |meth, &eval_block|
+        EverydayThorUtil::SubCommandHelpers.print_info(meth, &eval_block)
+      }
+    end
 
-      def add_helper(parent_class, v)
-        parent_class.no_commands { parent_class.create_method v[:options][:name].to_sym, &v[:block] } if v[:block]
-      end
-
-      def register_print_info_helper(global, helper_symbol, method_name, parent)
-        register(helper_symbol, name: (method_name || 'print_info'), global: global, parent: parent) { |meth, &eval_block|
-          EverydayThorUtil::SubCommandHelpers.print_info(meth, &eval_block)
-        }
-      end
-
-      def print_info(meth, &eval_block)
-        meth_obj = self.method(meth)
-        puts "command: #{self.class.basename2} #{meth.to_s}"
-        puts "parent_options: #{parent_options.inspect}"
-        puts "options: #{options.inspect}"
-        meth_obj.parameters.each { |p| puts "#{p[1].to_s}: #{eval_block.call(p[1].to_s)}" } if eval_block
-      end
+    def print_info(meth, &eval_block)
+      meth_obj = self.method(meth)
+      puts "command: #{self.class.basename2} #{meth.to_s}"
+      puts "parent_options: #{parent_options.inspect}"
+      puts "options: #{options.inspect}"
+      meth_obj.parameters.each { |p| puts "#{p[1].to_s}: #{eval_block.call(p[1].to_s)}" } if eval_block
     end
   end
 end
